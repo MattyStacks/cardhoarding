8bc6b7f825eb08a5f00c5331a0b5699555e414c2
Matt Therault <matt@Matts-MacBook-Pro.local>
2023-02-27 15:54:00 -0500

    added more favicons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SiteFooter/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SiteFooter from './components/SiteFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SiteFooter
   }
 }
 </script>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer> 
+    <p>&copy; cardhoarding.com | <a href="https://github.com/your-username/your-repo-name">Contribute on GitHub</a></p>
+    <p>The vast majority of this site has been built with ChatGPT and copilot, and other AI tools.</p>
+  </footer>
+</template>
+
+<style>
+footer {
+  background-color: #f2f2f2;
+  padding: 20px;
+  text-align: center;
+}
+</style>
